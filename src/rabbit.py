ce2d8cf64a7237a5babcdc553caedaa36b565e46
Andrés García <jose.andres.gm29@gmail.com>
2019-09-04 12:10:43 -0500

    add url connection to pika blocking connection

--- src/rabbit.py
+++ src/rabbit.py
@@ -1,20 +1,32 @@
+import os
+import pika
+
 def __connect():
     try:
-        url = os.environ['URL_AMPQ']
-        connection = pika.BlockingConnection()
+        url_connection = os.environ['URL_AMPQ']
+        connection = pika.BlockingConnection(pika.ConnectionParameters(url_connection))
         channel = connection.channel()
         return channel
     except:
         raise NameError('Error to connect')
 
-def sendMessage(body):
+def sendMessage(exchange, body):
     channel = __connect()
     try:
-        channel.basic_publish(exchange='direct', routing_key='test',
-                        body=b'Test message.')
+        channel.basic_publish(
+                        exchange=exchange_validator(exchange),
+                        routing_key='test',
+                        body=b'Test message.'
+                    )
         connection.close()
     except:
         raise NameError('Error to send message')
 
 def setUrl(url):
     os.environ['URL_AMPQ'] = url
+
+def exchange_validator(exchange):
+    valid_options = ['direct', 'fanout', 'topic', 'exchange']
+    if exchange not in valid_options:
+        raise NameError('The exchange is not valid')
+    return exchange
