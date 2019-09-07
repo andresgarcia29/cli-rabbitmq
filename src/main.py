9ff9b9fcd714152c513c15e59dcab9602e3f7dfd
Andrés García <jose.andres.gm29@gmail.com>
2019-09-07 16:46:29 -0500

    send message with rbmq and delete set connection

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
 import os
 import click
 import pika
-from rabbit import setUrl, sendMessage
+from rabbit import sendMessage
 
 @click.group()
 def cli():
     pass
 
 @click.command()
-@click.option('--url', '-u', default='', type=str, help='Url to connect with RabbitMq')
-def connect(url):
-    """Set the rabbitmq url to know where connect"""
-    setUrl(url)
-
-@click.command()
 @click.option('--exchange', '-e', default='direct', type=str, help='Choose the exchange.')
 @click.option('--queue', '-q', default='', type=str, help='Choose the queue name.')
 @click.option('--file', '-f', default='', type=str, help='Choose the json file.')
-@click.option('--save', '-s', default='', type=str, help='Save the json file.')
+@click.option('--save', '-s', is_flag=True, default=False, type=bool, help='Save the json file.')
 @click.option('--manually', '-m', is_flag=True, default=False, type=bool, help='Write manually.')
 @click.option('--counter', '-c', default=1, type=int, help='Repeat # times.')
 def send(exchange, queue, file, save, manually, counter):
--- src/rabbit.py
+++ src/rabbit.py
@@ -1,32 +1,39 @@
 import os
+import json
 import pika
 
 def __connect():
     try:
         url_connection = os.environ['URL_AMPQ']
-        connection = pika.BlockingConnection(pika.ConnectionParameters(url_connection))
+        connection = pika.BlockingConnection(pika.URLParameters(url_connection))
         channel = connection.channel()
         return channel
     except:
         raise NameError('Error to connect')
 
+def __save(file):
+    pass
+
 def sendMessage(exchange, queue, file, save, manually):
     channel = __connect()
+    
+    if save:
+        __save(file)
+    else:
+        body = json.dumps(open(file, 'r').read())
+
     try:
         channel.basic_publish(
                         exchange=exchange_validator(exchange),
-                        routing_key='test',
-                        body=b'Test message.'
+                        routing_key=queue,
+                        body=body
                     )
-        connection.close()
+        channel.close()
     except:
         raise NameError('Error to send message')
 
-def setUrl(url):
-    os.environ['URL_AMPQ'] = url
-
 def exchange_validator(exchange):
-    valid_options = ['direct', 'fanout', 'topic', 'exchange']
+    valid_options = ['', 'direct', 'fanout', 'topic', 'exchange']
     if exchange not in valid_options:
         raise NameError('The exchange is not valid')
-    return exchange
+    return 'amq.{0}'.format(exchange)
